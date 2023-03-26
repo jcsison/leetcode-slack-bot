7cb15068232fb496e25692936878bafa8caa0aa0
Jesser Sison <jesser.sison@live.com>
2023-03-26 12:19:39 -0700

    feat: update scopes

--- src/bolt/jobs/index.ts
+++ src/bolt/jobs/index.ts
@@ -1,4 +1,4 @@
-import { intervalPing } from './intervalPing.js';
+// import { intervalPing } from './intervalPing.js';
 import { postQuestion } from './postQuestion.js';
 
-export const jobs = [intervalPing, postQuestion];
+export const jobs = [postQuestion];
